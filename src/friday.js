32c83d6a496d523267b3e38faafcd50263f54f46
thatboytitz <mmejia1337@gmail.com>
2018-03-29 18:55:41 -0700

    Added Link and Admin Commands
    Display Links
    $links
    
    -----------------------------
    Admin Only
    -----------------------------
    $admin <add | remove> <User>
    $links <add | remove > <url> <url name>
    $links <change > <number <url>
    $links <move | swap> <location 1>  <location 2>(edited)

--- src/friday.js
+++ src/friday.js
@@ -1,12 +1,13 @@
 // Import the discord.js module
 const Discord = require('discord.js'),
-      config  = require('./config.json'),
-      characterList = require('./characters.json'),
-      typeList = require('./types.json')
+      config  = require('./data/config.json'),
+      characterList = require('./data/characters.json'),
+      typeList = require('./data/types.json')
+      fs = require('fs')
 // const 
 
 // Create an instance of a Discord client
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
 
 // The token of your bot - https://discordapp.com/developers/applications/me
 const token = config.token;
@@ -19,10 +20,10 @@ client.on('ready', () => {
 
 // Create an event listener for messages
 client.on('message', message => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot || message.channel.type === 'dm') return;
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   switch (command) {
     case "type" :
@@ -61,10 +62,95 @@ client.on('message', message => {
         message.channel.send({embed})
       }
       else {
-        message.channel.send(`${char} is not a valid character`)
+        message.channel.send(`${args.join(' ')} is not a valid character`)
       }
     break;
+    case 'l':
+    case 'link':
+    case 'links':
+      let [linkArg, url] = args;
+      let urlName = args.slice(2).join(' ')
+      if(linkArg === undefined) {
+        const embed = embedLinks()
+        return message.channel.send({embed})
+      } 
+      if(!isAdmin(message.member.id)){
+        return message.channel.send('Only Mods can add or modify links')
+      }
+      switch(linkArg){
+        case 'add':
+          if(url === undefined || !ValidURL(url)) return message.channel.send('Not a valid url')
+          if(urlName == '') return message.channel.send('Please add url name')
+          config.links.push({'name':urlName,'url':url})
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${urlName} has now been added to link ${config.links.length}`)
+        break;
+        case 'remove':
+          if(url === undefined) return message.channel.send(`Please add link number between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          let num = parseInt(url)
+          if(num < 1 || num > config.links.length) return message.channel.send(`Please add link number between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          let deletedLink = config.links[num - 1].name
+          config.links.splice(num - 1,1)
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${deletedLink} has now been removed`)
+        break;
+        case 'change': //FORGIVE ME FOR THE HACKY CODE I'M ABOUT TO DO url = num and urlName is url I'M SORRY
+          if(url === undefined) return message.channel.send(`Please add link number between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          let number = parseInt(url)
+          if(number < 1 || number > config.links.length) return message.channel.send(`Please add link number between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          if(urlName == '' || !ValidURL(urlName)) return message.channel.send('please add valid url')
+          config.links[number - 1].url = urlName
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${config.links[number - 1].name} now points to ${config.links[number - 1].url}`)
+        break;
+        case 'swap':
+          if(url === undefined) return message.channel.send(`Please add 2 link numbers between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          let num1 = parseInt(url)
+          let num2 = parseInt(urlName)
+          if(num1 < 1 || num1 > config.links.length || num2 < 1 || num2 > config.links.length) return message.channel.send(`Please add link number between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          swapArrayElements(config.links, num1 - 1, num2 - 1)
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${config.links[num2 -1].name} and ${config.links[num1 -1].name} have now switched places`)
+        break;
+        case 'move':
+          if(url === undefined) return message.channel.send(`Please add 2 link numbers between 1 - ${config.links.length > 25 ? `25` : `${config.links.length}`}`)
+          let number1 = parseInt(url)
+          let number2 = parseInt(urlName)          
+          let tempLink = config.links[number1 -1]
+          config.links.splice(number1 - 1, 1)
+          config.links.splice(number2 -1 , 0, tempLink)
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${config.links[number2 -1].name} is now link number ${number2}`)
+        break;
+      }   
+
+    break;
+    case 'a':
+    case 'admin':
+      if(message.member.id !== message.guild.ownerID && !message.member.hasPermission("ADMINISTRATOR") && !isAdmin(message.member.id)) return message.channel.send('you are not server owner');
+      let [adminArg, user] = args;
+      let toAdmin = message.mentions.users.first() || message.guild.members.get(user)
+      switch(adminArg){
+        case 'add':
+          if(!toAdmin) return message.channel.send("You did not not specify a user.")    
+          if(isAdmin(toAdmin.id || toAdmin.user.id)) return message.channel.send("User is already Admin.")
+          config.admin.push((toAdmin.id || toAdmin.user.id))
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${(toAdmin.username || toAdmin.user.username)} can now use bot admin commands.`)
+        break;
+        case 'remove':
+          if(!toAdmin) return message.channel.send("You did not not specify a user.")
+          if(!isAdmin(toAdmin.id || toAdmin.user.id)) return message.channel.send("User is not an Admin.")
+          let index = config.admin.indexOf(toAdmin.id || toAdmin.user.id)
+          config.admin.splice(index, 1)
+          writeToFile(config, `${__dirname}/data/config.json`)
+          return message.channel.send(`${(toAdmin.username || toAdmin.user.username)} can no longer use bot admin commands.`)
+        break;
+      }
+
+    break;
   }
+  return;
 });
 
 function getCharacter(prompt) {
@@ -116,6 +202,47 @@ function embedType(type){
   return(embed)
 }
 
+function embedLinks(){
+  const embed = new Discord.RichEmbed()
+    .setTitle('Important Links')
+  if(config.links.length > 0){
+    for (let i = 0; i < config.links.length && i < 25; i++) {
+      embed.addField(`${i+1}. ${config.links[i].name}`, config.links[i].url) 
+    }
+  }
+  else {
+    embed.setDescription(`There are no links`)
+  }
+  return embed
+}
+
+function isAdmin(element) { return config.admin.indexOf(element) > -1}
+
+function writeToFile(element, location){
+  json = JSON.stringify(element,null, 2)
+  fs.writeFile(location, json, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}
+
+function ValidURL(str) {
+  // var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  // '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
+  // '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  // '(\\:\\d+)?'+ // port
+  // '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
+  // '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
+  // '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  // return pattern.test(str);
+  return true;
+}
+
+var swapArrayElements = function(arr, indexA, indexB) {
+  var temp = arr[indexA];
+  arr[indexA] = arr[indexB];
+  arr[indexB] = temp;
+};
 
 // Log our bot in
 client.login(token);
