69e4ab63fba33fd0326781a6bf6cbc6d41fd0cff
thwips <869802+thwips@users.noreply.github.com>
2018-08-05 23:30:48 -0700

    added main board hero command
    shows image of farmable hero shards locations

---
+++ src/cmds/mainheroes.js
@@ -0,0 +1,20 @@
+const gf = require('../globalfunctions.js')
+
+module.exports.run = async (bot, message, args) => {
+  if(!gf.isAllowed(message, module.exports.help.ignore)) return;
+  return message.channel.send({
+    file: 'https://i.imgur.com/g62DGoi.png'
+  })
+}
+
+module.exports.help = {
+  name: "main",
+  aliases: [
+    "mainheroes",
+    "main",
+    "m"
+  ],
+  ignore: [
+  "429222755745923073"
+  ]
+}
