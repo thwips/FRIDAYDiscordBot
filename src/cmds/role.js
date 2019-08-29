8ce75640d06209d5f0ff5547b19fdffed1d99d30
thwips <869802+thwips@users.noreply.github.com>
2019-08-29 13:55:38 -0700

    Added Dragalia
    Added Dragalia to list of accepted roles

--- src/cmds/role.js
+++ src/cmds/role.js
@@ -1,7 +1,7 @@
 const gf = require('../globalfunctions.js'),
       fs = require('fs')
 
-let gameRoles = ['FE:H', 'MFF', 'ACPC', 'Pokemon Masters'];
+let gameRoles = ['FE:H', 'MFF', 'ACPC', 'Pokemon Masters', 'Dragalia'];
 
 module.exports.run = async (bot, message, args) => {
   if(!gf.isAllowed(message, module.exports.help.ignore)) return;
