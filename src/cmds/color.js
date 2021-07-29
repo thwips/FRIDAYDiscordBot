3323f82551f9b576a39531ea48a85fc95466ec71
thwips <mmejia1337@gmail.com>
2021-07-29 01:50:47 +0000

    commiting from server

--- src/cmds/color.js
+++ src/cmds/color.js
@@ -6,45 +6,52 @@ let colorRoles = ['Guardians 3000', 'Discord Defenders', 'Thunderbolts Founder',
 module.exports.run = async (bot, message, args) => {
   if(!gf.isAllowed(message, module.exports.help.ignore)) return;
 
-  let serverRoles = message.guild.roles.filter(role => colorRoles.includes(role.name));
-  if (serverRoles.size < 1)
-  {
-    return;
-  }
+  message.guild.roles.fetch()
+    .then(serverRoles => {
+      serverRoles = serverRoles.cache.filter(role => colorRoles.includes(role.name));
+      if (serverRoles.size < 1)
+      {
+        return;
+      }
 
-  let str = args.join(' ').trim();
-  if (args.join('') == 'show')
-  {
-    message.channel.send('Role options: ' + serverRoles.array().join(', '));
-    return;
-  }
+      let str = args.join(' ').trim();
+      if (args.join('') == 'show')
+      {
+        message.channel.send('Role options: ' + serverRoles.array().join(', '));
+        return;
+      }
 
-  let roles = serverRoles.filter(role =>
-  {
-    return role.name.toLowerCase().startsWith(str.toLowerCase());
-  });
+      let roles = serverRoles.filter(role =>
+      {
+        return role.name.toLowerCase().startsWith(str.toLowerCase());
+      });
 
-  if (roles.size > 1)
-  {
-    message.channel.send('Inputted role not specific enough. Not assign role.\nRoles found: ' + roles.array().join(', '));
-    return;
-  }
-  else if (roles.size == 0)
-  {
-    message.channel.send('Cannot find role starting with ' + str)
-    return;
-  }
-  let role = roles.first();
-  try
-  {
-    await message.member.removeRoles(serverRoles);
-    await message.member.addRole(role);
-    message.channel.send(`You now have the color ${role}!`);
-  }
-  catch (e)
-  {
-    message.channel.send(`Operation failed! ${e.message}`);
-  }
+      if (roles.size > 1)
+      {
+        message.channel.send('Inputted role not specific enough. Not assign role.\nRoles found: ' + roles.array().join(', '));
+        return;
+      }
+      else if (roles.size == 0)
+      {
+        message.channel.send('Cannot find role starting with ' + str)
+        return;
+      }
+      let role = roles.first();
+      try
+      {
+        message.member.roles.remove(serverRoles)
+          .then(async () =>
+          {
+            await message.member.roles.add(role)
+            message.channel.send(`You now have the color ${role}!`);
+          })
+      }
+      catch (e)
+      {
+        message.channel.send(`Operation failed! ${e.message}`);
+      }
+    })
+    .catch(console.error);
 }
 
 module.exports.help = {
